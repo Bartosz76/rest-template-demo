b9fd371620a86a69c5954df538b3cb387ed06d59
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-18 19:54:22 +0200

    Application now fetches a random quote about Spring from an external resource thanks to Rest Template.

--- src/main/java/bm/app/resttemplatedemo/RestTemplateDemoApplication.java
+++ src/main/java/bm/app/resttemplatedemo/RestTemplateDemoApplication.java
@@ -1,13 +1,35 @@
 package bm.app.resttemplatedemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestTemplateDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestTemplateDemoApplication.class, args);
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(RestTemplateDemoApplication.class);
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 
 }
